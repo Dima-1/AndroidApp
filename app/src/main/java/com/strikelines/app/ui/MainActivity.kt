f335f13bacc9434f08cf9b55a1ed93d268abade8
Chumva <faiver.unknown@gmail.com>
2019-05-03 16:41:30 +0300

    Enable online data fir maps

--- app/src/main/java/com/strikelines/app/ui/MainActivity.kt
+++ app/src/main/java/com/strikelines/app/ui/MainActivity.kt
@@ -426,6 +426,7 @@ class MainActivity : AppCompatActivity(), OsmandHelperListener {
 				putBoolean("driving_region_automatic", false)
 				putString("default_metric_system", METRIC_CONST_NAUTICAL_MILES)
 				putString("default_speed_system", SPEED_CONST_NAUTICALMILES_PER_HOUR)
+				putBoolean("map_online_data", true)
 			}
 			customizeOsmandSettings("strikelines", bundle)
 		}
